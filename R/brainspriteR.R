18ee0979570304f86f9fb14d4d8db0a8a3638012
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-08-07 16:00:30 -0700

    overlay and temporary solution to id collusions

--- R/brainspriteR.R
+++ R/brainspriteR.R
@@ -1,27 +1,47 @@
 #' @export
 brainsprite = function(sprites,
-                        spriteHeight = 233,
-                        spriteWidth = 189,
-                        id = '3Dviewer',
-                        spriteID = 'spriteImg',
-                        height = NULL,
-                        width = NULL){
+                       spriteHeight = 233,
+                       spriteWidth = 189,
+                       id = tempfile() %>% basename,
+                       spriteID = tempfile() %>% basename,
+                       overlay = NULL,
+                       overlayHeight = NULL,
+                       overlayWidth = NULL,
+                       overlayID = tempfile() %>% basename,
+                       overlayOpacity = 0.5,
+                       height = NULL,
+                       width = NULL){
+
+    script = glue::glue(" $( window ).load(function() {
+                       var brain = brainsprite({
+                       canvas: '[id]', // That is the ID of the canvas to build slices into
+                       sprite: '[spriteID]', // That is the ID of the sprite image that includes all (sagital) brain slices
+                       nbSlice: { 'Y':[spriteHeight] , 'Z':[spriteWidth] }"
+                       ,.open = '[',.close = ']')
+
+    if(!is.null(overlay)){
+        script = glue::glue(script,",
+                   overlay: {
+                       sprite: '[overlayID]',
+                       nbSlice:  { 'Y':[overlayHeight] , 'Z':[overlayWidth] }
+                   }",.open = '[',.close = ']')
+    }
+
+    end = "});
+});"
+
+    script = paste0(script,end)
 
     out = htmltools::tagList(htmltools::div(
         glue::glue('<canvas id="{id}">') %>% htmltools::HTML(),
         htmltools::img(id = spriteID,src = sprites, class = 'hidden'),
+        htmltools::img(id = overlayID,src = overlay, class = 'hidden'),
         height = height,
         width = width),
         htmltools::includeScript(system.file('inst/brainsprite/brainsprite.js',package = 'brainspriteR')),
         htmltools::includeScript(system.file('inst/brainsprite/jquery.min.js',package = 'brainspriteR')),
-        htmltools::tags$script(
-            glue::glue(" $( window ).load(function() {
-                       var brain = brainsprite({
-                       canvas: '[id]', // That is the ID of the canvas to build slices into
-                       sprite: '[spriteID]', // That is the ID of the sprite image that includes all (sagital) brain slices
-                       nbSlice: { 'Y':[spriteHeight] , 'Z':[spriteWidth] }
-                       });
-});",.open = '[',.close = ']')
+        htmltools::tags$script(script
+
         ))
 
     class(out) = append(class(out),'brainsprite')
