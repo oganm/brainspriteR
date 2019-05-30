eb6030a7fb6cd5dd726156403cbd20e90b3bc4f7
OganM <ogan.mancarcii@gmail.com>
2019-05-29 21:54:39 -0700

    proper use of javascript resources and documentation

--- R/brainspriteR.R
+++ R/brainspriteR.R
@@ -1,3 +1,23 @@
+#' Brainsprite
+#'
+#' Creates an interactive 3D viewer for MRI slices.
+#'
+#' @param sprites File path for the spries
+#' @param spriteHeight Height of the brain sprite tiles
+#' @param spriteWidth Width of the brain sprite tiles
+#' @param inputId Input ID for use in shiny applications
+#' @param spriteID Id for the sprite image
+#' @param flagCoordinates Show coordinates of the clicked position
+#' @param origin Which voxel has the coordinates 0,0,0
+#' @param fontColor Color of the text
+#' @param background Background color
+#' @param overlay File path for an optional overlay image
+#' @param overlayHeight Height of the overlay sprite tiles
+#' @param overlayWidth Width of the overlay sprite tiles
+#' @param overlayID Id for the overlay image
+#' @param overlayOpacity Opacity of the overlay image
+#' @param height Height of the visualization
+#' @param width Width of the visualization
 #' @export
 brainsprite = function(sprites,
                        spriteHeight = 233,
@@ -15,6 +35,8 @@ brainsprite = function(sprites,
                        overlayOpacity = 0.5,
                        height = NULL,
                        width = NULL){
+
+
 
     script = glue::glue(" $( <spriteID> ).load(function() {
                        var brain = brainsprite({
@@ -49,20 +71,42 @@ brainsprite = function(sprites,
         htmltools::img(id = overlayID,src = overlay),
         height = height,
         width = width),
-        htmltools::includeScript(system.file('brainsprite/brainsprite.js',package = 'brainspriteR')),
-        htmltools::includeScript(system.file('brainsprite/jquery.min.js',package = 'brainspriteR')),
+        htmltools::htmlDependency('brainsprite',
+                                  src = system.file('brainsprite',package = 'brainspriteR'),
+                                  version = '1.0',
+                                  script = c('brainsprite.js',
+                                             'jquery.min.js')),
         htmltools::tags$script(script))
 
+    attributes(out) = append(attributes(out), list(sprites = sprites,
+                                                   overlay = overlay,
+                                                   spriteID = spriteID,
+                                                   overlayID = overlayID))
     class(out) = append('brainsprite',class(out))
 
     return(out)
 }
 
 #' @export
-print.brainsprite = function(x){
-    tempFile <- tempfile()
-    tempFile = 'hede.html'
-    viewer <- getOption("viewer")
-    writeLines(paste0('<html>\n',as.character(x),'\n</html>'),tempFile)
-    viewer(tempFile)
+print.brainsprite = function(x, viewer = getOption("viewer", utils::browseURL)){
+    www_dir <- tempfile("viewhtml")
+    dir.create(www_dir)
+    index_html <- file.path(www_dir, "index.html")
+    attr = attributes(x)
+
+    file.copy(attr$sprites,glue::glue("{www_dir}/sprites"))
+    file.copy(attr$overlay,glue::glue("{www_dir}/overlay"))
+
+    x[[1]]$children[[2]] = htmltools::img(id = attr$spriteID,src = 'sprites')
+
+    if(!is.null(attr$overlay)){
+        x[[1]]$children[[3]] = htmltools::img(id = attr$overlayID,src = 'overlay')
+    }
+
+    htmltools::save_html(x, file = index_html, background = 'white',
+              libdir = "lib")
+    if (!is.null(viewer))
+        viewer(index_html)
+    invisible(index_html)
+
 }
--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 
 This is an R wrapper for [brainsprite.js](https://github.com/SIMEXP/brainsprite.js).
 
-See [here](https://oganm.github.io/brainspriteR/demo) for a demo
+It is a visualization tool for MRI images. Given a sprite sheet like [this](docs/sprite.jpg), it will create a interactive 3D viewer.
+
+## Installation
+
+```r
+devtools::install_github('oganm/brainspriteR)
+
+```
+
+## Usage
+
+```r
+brainsprite(sprites = 'sprite.jpg',
+            flagCoordinates = TRUE,
+            spriteHeight = 233,
+            spriteWidth = 189)
+
+```
+![](example.png)
+
+See [here](https://oganm.github.io/brainspriteR/demo) for an interactive demo.
+
